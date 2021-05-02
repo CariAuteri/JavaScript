994e02bc0fff94b73025e9d2c0fc906c75cefb03
Carina Auteri <autericarina@gmail.com>
2021-05-02 19:31:45 -0300

    Tarea Finalizada

---
+++ Tareas/Primeras Tareas/tarea2804.js
@@ -0,0 +1,23 @@
+//Tarea 1
+let nombre= 'jose'
+console.log(nombre.charAt(0).toUpperCase()+nombre.slice(1))
+
+//Tarea 2
+let number1=1212
+let number2=405
+let number3=10.55
+
+console.log(`Número Máximo: ${Math.max(1212,405,10.55)}`)
+console.log(`Número Minímo: ${Math.min(1212,405,10.55)}`)
+console.log(Math.sqrt(405))
+
+//Tarea 3
+let number4=2017
+let number5=817
+
+console.log(number4 > number5)
+let suma= number4 + number5
+console.log(number4 < number5)
+let resta= number4 - number5
+console.log(number4 == number5)
+let multiplicacion= number4 * number5
