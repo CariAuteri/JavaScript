53a2d1f59ef2916d7480b9aa784f61cfd3808e65
Carina Auteri <autericarina@gmail.com>
2021-05-02 17:30:14 -0300

    agregando nueva tarea

---
+++ Tareas/Primeras Tareas/tarea2604.js
@@ -0,0 +1,11 @@
+let number1=32
+let number2=12.12
+
+let suma= numer1 + number2
+let resta= number1 - number2
+let multiplicar= number1 * number2
+let division= number1 / number2
+
+console.log(number1 >= number2)
+console.log(number1 == number2)
+
